bd6d5fdafcab021de9dba0d8a7fc425c80bf49da
Robert Ostensen <robert.ostensen@saplinglearning.com>
2017-01-18 16:00:38 -0600

    add lint, update docs

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
 #ldap proxy to aws iam
 
-ensure aws credentials are available, possibly via IAM Role to ec2 instance
+ensure aws credentials are available, possibly via IAM Role on ec2 instance
 
-set GROUP_NAME,DOMAIN,DOMAIN and optionally DEFAULT_GID, DEFAULT_UID, SECRET and PORT
-## example
+set AWS_REGION, GROUP_NAME, DOMAIN 
+and optionally DEFAULT_GID, REQUIRE_UID, REQUIRE_GID, SECRET and PORT
+
+## example for testing
 >  export GROUP_NAME=MyUsers
 >  export AWS_REGION=us-east-1
 >  export DOMAIN=example.com
 >  export SECRET=ldaprootaccountpasswordhere
+>  export DEFAULT_GID=500
+>  export REQUIRE_UID=$UID
+>  export PORT=/var/run/ldap.sock
 
-then run node index.js or npm start
+then run `node index.js` or `npm start`
 
-connect with simple binding as cn=root with the secret as the password
+## testing the service
+the domain gets split into "DC=" based on the dots so example.com turns into 
+dc=example,dc=com test.ldap.fred turns into dc=test,dc=ldap,dc=fred
+
+```
+ldapsearch -H ldapi://%2fvar%2frun%2fldap.sock/ -x -b ou=users,dc=example,dc=com objectclass=\*
+```
+
+connect to a unix socket as a specified user or group *or* 
+connect with simple binding as cn=root with the secret as the password 
 
 ## NB port may be a file which should create a unix socket
-when the using unix socket connection the process connecting will be validated against
-the DEFAULT_GID and DEFAULT_UID *at least* one must be set
+when the using unix socket connection the process connecting will be validated 
+against the REQUIRE_UID and REQUIRE_GID *at least* one must be set.  When using 
+a TCP port if you have not set a SECRET one will be created for you and printed 
+to STDOUT on each run
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 const fs = require('fs')
 const ldap = require('ldapjs')
 const AWS = require('aws-sdk')
@@ -11,61 +11,60 @@ const iamEpoch = (new Date('2010-01-01')).getTime()
 
 // get as much uniqness as possible from a short string
 // this produces 3 hashes with more that 75 collisions from /usr/share/dict/words
-var shortHash = (s) => String(s).split('').reduce((p,c)=>p^Math.pow(c.charCodeAt(),2), 0x3fff)
+const shortHash = (s) => String(s).split('').reduce((p, c) => p ^ Math.pow(c.charCodeAt(), 2), 0x3fff)
 
-var secret
-if ( process.env.SECRET ) {
+let secret
+
+if (process.env.SECRET) {
   secret = process.env.SECRET
 } else {
-  hash.update(Array.from('0123456789').map(a=>Math.floor(Math.random() * 100)).join(''))
+  hash.update(Array.from('0123456789').map(a => Math.floor(Math.random() * 100)).join(''))
   secret = hash.digest('hex')
+  console.log('using secret:', secret)
 }
-
-console.log ( 'using secret:', secret )
 
 AWS.config.update({region: process.env.AWS_REGION})
 
-var iam = new AWS.IAM({apiVersion: '2010-05-08'})
+let iam = new AWS.IAM({apiVersion: '2010-05-08'})
 
-var server = ldap.createServer()
+let server = ldap.createServer()
 
-const domain = process.env.DOMAIN.split('.').map( dc => 'dc=' + dc.toLowerCase() ).join(',')
+const domain = process.env.DOMAIN.split('.').map(dc => 'dc=' + dc.toLowerCase()).join(',')
 
-function authorize(req, res, next) {
-  if (isNaN(parseInt(process.env.PORT))){
-    let credentials=peercred.fromSock(req.connection)
-    if ((process.env.REQUIRE_UID && credentials.uid == process.env.REQUIRE_UID) ||
-        (process.env.REQUIRE_GID && credentials.gid == process.env.REQUIRE_GID)) {
+function authorize (req, res, next) {
+  if (isNaN(parseInt(process.env.PORT))) {
+    let credentials = peercred.fromSock(req.connection)
+    if ((process.env.REQUIRE_UID && credentials.uid === parseInt(process.env.REQUIRE_UID)) ||
+        (process.env.REQUIRE_GID && credentials.gid === parseInt(process.env.REQUIRE_GID))) {
       return next()
     } else {
-      console.log ('UID or GID mismatch',credentials)
+      console.log('UID or GID mismatch', credentials)
       return next(new ldap.InsufficientAccessRightsError())
     }
   }
-  if (!req.connection.ldap.bindDN.equals('cn=root')){
+  if (!req.connection.ldap.bindDN.equals('cn=root')) {
     console.log('user not bound or insufficent rights, try as cn=root')
     return next(new ldap.InsufficientAccessRightsError())
   }
   return next()
 }
 
-server.bind('cn=root', function(req, res, next) {
-  console.log ( req.credentials )
-  if (req.dn.toString() !== 'cn=root' || req.credentials !== secret)
+server.bind('cn=root', function (req, res, next) {
+  if (req.dn.toString() !== 'cn=root' || req.credentials !== secret) {
     return next(new ldap.InvalidCredentialsError())
+  }
 
-  res.end()
-  return next()
-}) 
-
-server.bind('cn=local', function(req, res, next) {
   res.end()
   return next()
 })
 
+server.bind('cn=local', function (req, res, next) {
+  res.end()
+  return next()
+})
 
 function getUsers (req, res, next) {
-  if (! process.env.GROUP_NAME) {
+  if (!process.env.GROUP_NAME) {
     return next()
   }
   if (!req.users) {
@@ -75,17 +74,16 @@ function getUsers (req, res, next) {
   if (req.marker) {
     opts.Marker = req.marker
   }
-  iam.getGroup(opts, (err, data ) => {
+  iam.getGroup(opts, (err, data) => {
     if (err) {
       console.log(err)
       return next(new ldap.UnavailableError())
     }
-    let uid = 68000 // more than 16bit max
     data.Users
-      .sort((a,b) => a.CreateDate > b.CreateDate) //new users last, only a problem when someone gets deleted
-      .forEach ( user => {
-        let username = user.Path.replace(/\//g,'')
-        if ( username.length ) {
+      .sort((a, b) => a.CreateDate > b.CreateDate) // new users last, only a problem when someone gets deleted
+      .forEach(user => {
+        let username = user.Path.replace(/\//g, '')
+        if (username.length) {
           req.users[username] = {
             dn: 'cn=' + username + ',ou=users,' + domain,
             attributes: {
@@ -97,58 +95,58 @@ function getUsers (req, res, next) {
               homedirectory: '/home/' + username,
               uid: username,
               // best effort at unique uids above 16bits
-              uidNumber: Math.floor((user.CreateDate.getTime()-iamEpoch)/1000)^shortHash(user.UserName), 
+              uidNumber: Math.floor((user.CreateDate.getTime() - iamEpoch) / 1000) ^ shortHash(user.UserName),
               gidNumber: process.env.DEFAULT_GID || 500,
-              objectclass: 'unixUser',
-              objectclass: 'posixAccount'
+              objectclass: ['unixUser', 'posixAccount']
             }
           }
         }
       })
     if (data.IsTruncated) {
       req.marker = data.Marker
-      getUsers (req, res, next)
+      getUsers(req, res, next)
     } else {
       next()
     }
   })
 }
 
-server.search(domain, authorize, getUsers, function(req, res, next) {
+server.search(domain, authorize, getUsers, function (req, res, next) {
   if (!req.users) {
     console.log('no users found')
     return next(new ldap.UnavailableError())
   }
-  Object.keys(req.users).forEach(function(k) {
-    if (req.filter.matches(req.users[k].attributes))
+  Object.keys(req.users).forEach(function (k) {
+    if (req.filter.matches(req.users[k].attributes)) {
       res.send(req.users[k])
+    }
   })
 
   res.end()
 })
 
 function start () {
-  server.listen(process.env.PORT || 1389, function() {
+  server.listen(process.env.PORT || 1389, function () {
     console.log('ldapjs listening at ' + server.url)
-    console.log ( Math.abs(parseInt(process.env.REQUIRE_UID || process.getuid())),
-        Math.abs(parseInt(process.env.REQUIRE_GID || process.getgid())), 'woo' )
+    console.log(Math.abs(parseInt(process.env.REQUIRE_UID || process.getuid())),
+        Math.abs(parseInt(process.env.REQUIRE_GID || process.getgid())), 'woo')
     if (isNaN(parseInt(process.env.PORT))) {
       fs.chown(
         process.env.PORT,
         parseInt(process.env.REQUIRE_UID || process.getuid()),
         parseInt(process.env.REQUIRE_GID || process.getgid()),
-        ()=>{})
+        () => {})
       fs.chmod(
         process.env.PORT,
         parseInt('0775', 8),
-        ()=>{})
+        () => {})
     }
   })
 }
 
 if (isNaN(parseInt(process.env.PORT))) {
   // attempt to remove socket file, ignore exceptions
-  fs.unlink(process.env.PORT,()=>start())
+  fs.unlink(process.env.PORT, () => start())
 } else {
   start()
 }
